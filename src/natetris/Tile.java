52ea82c19bb3d8d43e160ab5e3745e9825a47959
Streppel <streppels@gmail.com>
2014-06-23 22:48:04 -0300

    Finished defining tiles logic bodies

--- src/natetris/Tile.java
+++ src/natetris/Tile.java
@@ -34,24 +34,138 @@ public enum Tile {
 		
 	TileO(new boolean[][] {
 			{
-				true,	true, 	true, 	true,
-				true,	true,	true,	true
+				true,	true, 
+				true,	true
 			},
 			{
-				true,	true, 	true, 	true,
-				true,	true,	true,	true
+				true,	true,
+				true,	true
 			},
 			{
-				true,	true, 	true, 	true,
-				true,	true,	true,	true
+				true,	true, 
+				true,	true
 			},
 			{
-				true,	true, 	true, 	true,
-				true,	true,	true,	true
-			},
-	});
+				true,	true,
+				true,	true
+			}
+	}),
 	
-	// TODO TileL, TileJ, TileS, TileZ, TileT
+	TileL(new boolean[][] {
+			{
+				false,	true,	false,
+				false,	true,	false,
+				false,	true,	true
+			},
+			{
+				false,	false,	false,
+				true,	true,	true,
+				true,	false,	false
+			},
+			{
+				true,	true,	false,
+				false,	true,	false,
+				false,	true,	false
+			},
+			{
+				false,	false,	true,
+				true,	true,	true,
+				false,	false,	false
+				
+			}
+	}), 
+	
+	TileJ(new boolean[][] {
+			{
+				false,	true,	false,
+				false,	true,	false,
+				true,	true,	false
+			},
+			{
+				true,	false,	false,
+				true,	true,	true,
+				false,	false,	false
+			},
+			{
+				false,	true,	true,
+				false,	true,	false,
+				false,	true,	false
+			},
+			{
+				false,	false,	false,
+				true,	true,	true,
+				false,	false,	true
+			}
+	}),
+	
+	TileS(new boolean[][] {
+			{
+				false,	true,	true,
+				true,	true,	false,
+				false,	false,	false
+			},
+			{
+				true,	false,	false,
+				true,	true,	false,
+				false,	true,	false
+			},
+			{
+				false,	false,	false,
+				false,	true,	true,
+				true,	true,	false
+			},
+			{
+				false,	true,	false,
+				false,	true,	true,
+				false,	false,	true
+			}
+	}),
+	
+	TileZ(new boolean[][] {
+			{
+				true,	true,	false,
+				false,	true,	true,
+				false,	false,	false
+			},
+			{
+				false,	false,	true,
+				false,	true,	true,
+				false,	true,	false
+			},
+			{
+				false,	false,	false,
+				true,	true,	false,
+				false,	true,	true
+			},
+			{
+				false,	true,	false,
+				true,	true,	false,
+				true,	false,	false
+			}	
+	}),
+	
+	TileT(new boolean[][] {
+			{
+				true,	true,	true,
+				false,	true,	false,
+				false,	true,	false
+			},
+			{
+				false,	false,	true,
+				true,	true,	true,
+				false,	false,	true
+			},
+			{
+				false,	true,	false,
+				false,	true,	false,
+				true,	true,	true
+			},
+			{
+				true,	false,	false,
+				true,	true,	true,
+				true,	false,	false
+			}	
+	});
 	
 	/**
 	 * How the tiles are set on the piece
