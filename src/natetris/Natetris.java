6533264428f2b41398fa1dd58c194054a8ddaaa8
Streppel <streppels@gmail.com>
2014-06-30 01:14:05 -0300

    added some prototyped key event handling

--- src/natetris/Natetris.java
+++ src/natetris/Natetris.java
@@ -1,6 +1,8 @@
 package natetris;
 
 import java.awt.BorderLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 
@@ -40,6 +42,63 @@ public class Natetris extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
 		
+		addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				switch (e.getKeyCode()) {
+					// start new game
+					case KeyEvent.VK_ENTER:
+						
+						break;
+						
+					// move down
+					case KeyEvent.VK_NUMPAD2:
+					case KeyEvent.VK_S:
+					case KeyEvent.VK_DOWN:
+						
+						break;
+						
+					// move right
+					case KeyEvent.VK_NUMPAD6:
+					case KeyEvent.VK_D:
+					case KeyEvent.VK_RIGHT:
+						
+						break;
+						
+					// move left
+					case KeyEvent.VK_NUMPAD4:
+					case KeyEvent.VK_A:
+					case KeyEvent.VK_LEFT:
+						
+						break;
+						
+					// move up
+					case KeyEvent.VK_NUMPAD8:
+					case KeyEvent.VK_W:
+					case KeyEvent.VK_UP:
+						
+						break;
+						
+					// pause
+					case KeyEvent.VK_P:
+						
+						break;
+						
+				}
+			}
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				/* empty block */
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				/* empty block */
+			}
+		});
+		
 		this.board = new Board(this);
 		this.infoPanel = new InfoPanel(this);
 		
@@ -55,6 +114,8 @@ public class Natetris extends JFrame {
 	 * This will refresh the JPanels and handle the game's logic. 
 	 */
 	public void startGame() {
+		setNewGame(true);
+		
 		
 	}
 	
