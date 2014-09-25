a01258059b2011a96d460bd300d5455a9316c205
Streppel <streppels@gmail.com>
2014-09-25 00:23:47 -0300

    some improvements on readibility; our lovely prediction square now shows next piece to come

--- src/natetris/InfoPanel.java
+++ src/natetris/InfoPanel.java
@@ -9,84 +9,101 @@ import javax.swing.JPanel;
 
 public class InfoPanel extends JPanel {
 
-	private static final long serialVersionUID = -3849098449596016270L;
-	
-	/**
-	 * Fonts used 
-	 */
-	private static final Font LARGE_FONT = new Font("Tahoma", Font.PLAIN, 16);
-	private static final Font SMALL_FONT = new Font("Tahoma", Font.PLAIN, 13);
-	
-	/**
-	 * Padding used for next piece's box
-	 */
-	private static final int PADDING = 5;
-	
-	/**
-	 * Invisible panel's border 
-	 */
-	private static final int BORDER_WIDTH = 5;
-	
-	/**
-	 * The biggest size that a piece may have
-	 */
-	private static final int BIGGEST_PIECE_SIZE = 4;
-	
-	/**
-	 * The space between the panel's strings. Useful for setting information strings dynamically.
-	 */
-	private static final int SPACE_BETWEEN_STRINGS = LARGE_FONT.getSize() + 10;
-	
-	/**
-	 * The game instance
-	 */
-	private Natetris natetris;
-	
-	public InfoPanel(Natetris natetris) {
-		this.natetris = natetris;
-		
-		setPreferredSize(new Dimension(Board.PANEL_WIDTH, Board.PANEL_HEIGHT));
-		setBackground(Color.BLACK);
-	}
-	
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		
-		g.setColor(Color.WHITE);
-		g.setFont(LARGE_FONT);
-		
-		int offset = SPACE_BETWEEN_STRINGS;
-		
-		/*
-		 * 
-		 */
-		String nextPieceMessage = "Next piece: ";
-		
-		g.drawRect(g.getFontMetrics().stringWidth(nextPieceMessage), offset / 2, getPredictionSquareEdge(), getPredictionSquareEdge());
-		g.drawString(nextPieceMessage, 0, offset += SPACE_BETWEEN_STRINGS);
-		
-		offset += getPredictionSquareEdge(); // strings have to be underneath the prediction square to be visible
-		
-		String points = "Points: ";
-		g.drawString(points, 0, offset);
-		g.drawString(Long.toString(natetris.getScore()), g.getFontMetrics().stringWidth(points), offset);
-		offset += SPACE_BETWEEN_STRINGS;
-		
-		g.setFont(SMALL_FONT);
-		String movePointersTip = "Move pieces using the pointers keys, OR:";
-		String moveLeftTip = "	move left by pressing 'A'";
-		String moveRightTip = "	move right by pressing 'D'";
-		String moveDownTip = "	move down by pressing 'S'";
-		g.drawString(movePointersTip, 0, offset += SPACE_BETWEEN_STRINGS);
-		g.drawString(moveLeftTip, 0, offset += SPACE_BETWEEN_STRINGS);
-		g.drawString(moveRightTip, 0, offset += SPACE_BETWEEN_STRINGS);
-		g.drawString(moveDownTip, 0, offset += SPACE_BETWEEN_STRINGS);
+  private static final long serialVersionUID = -3849098449596016270L;
+  
+  /**
+   * Fonts properties
+   */
+  private static final Font LARGE_FONT = new Font("Tahoma", Font.PLAIN, 16);
+  private static final Font SMALL_FONT = new Font("Tahoma", Font.PLAIN, 13);
+  private static final Color FONT_COLOR = Color.GRAY;
+  
+  /**
+   * The biggest size that a piece may have (piece I)
+   */
+  private static final int MAX_PIECE_DIMENSION = 4;
+  
+  /**
+   * Constants used through the panel for layout
+   */
+  private static final int TILE_SIZE = Board.TILE_SIZE;
+  private static final int SPACE_BETWEEN_STRINGS = 25;
+  private static final int PADDING_5 = 5;
+  private static final int BORDER_WIDTH = Board.BORDER_WIDTH;
+  
+  /**
+   * The game instance
+   */
+  private Natetris natetris;
+  
+  public InfoPanel(Natetris natetris) {
+    this.natetris = natetris;
+    
+    setPreferredSize(new Dimension(Board.PANEL_WIDTH, Board.PANEL_HEIGHT));
+    setBackground(Color.BLACK);
+  }
+  
+  @Override
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    g.setColor(Color.WHITE);
+    g.setFont(LARGE_FONT);
+    int offset = SPACE_BETWEEN_STRINGS;
+    /*
+     * this enhances drawing methods readability, by considering the 
+     * 0,0 axis the beginning of the border limit of the board
+     */
+    g.translate(BORDER_WIDTH, BORDER_WIDTH);
+    
+    /*
+     * Draws both the rectangle which will hold the next piece to come and
+     * the piece itself 
+     */
+    g.drawRect(0, 0, predictionSquareWidth(), predictionSquareWidth());
+    if (natetris.isGameRunning()) {
+      Piece piece = natetris.getNextPiece();
+      
+      int dim = piece.getDimension();
+      
+      for (int x = 0; x < dim; x++) {
+        for (int y = 0; y < dim; y++) {
+          if (piece.isTile(x, y, 0)) {
+        	  int currX = PADDING_5 + (x * TILE_SIZE);
+        	  int currY = PADDING_5 + (y * TILE_SIZE);
+	          drawTile(piece, currX, currY, g);
+          }
+        }
+      }
+    }
+    
+    g.setColor(FONT_COLOR);
+    offset += predictionSquareWidth(); // strings have to be underneath the prediction square to be visible
+    
+    String points = "Points: ";
+    g.drawString(points, 0, offset);
+    g.drawString(Long.toString(natetris.getScore()), g.getFontMetrics().stringWidth(points), offset);
+    offset += SPACE_BETWEEN_STRINGS;
+    
+    g.setFont(SMALL_FONT);
+    String movePointersTip = "Move pieces using the pointers keys, OR:";
+    String moveLeftTip = "  move left by pressing 'A'";
+    String moveRightTip = " move right by pressing 'D'";
+    String moveDownTip = "  move down by pressing 'S'";
+    g.drawString(movePointersTip, 0, offset += SPACE_BETWEEN_STRINGS);
+    g.drawString(moveLeftTip, 0, offset += SPACE_BETWEEN_STRINGS);
+    g.drawString(moveRightTip, 0, offset += SPACE_BETWEEN_STRINGS);
+    g.drawString(moveDownTip, 0, offset += SPACE_BETWEEN_STRINGS);
 
-	}
-	
-	private int getPredictionSquareEdge() {
-		return Board.TILE_SIZE * BIGGEST_PIECE_SIZE + PADDING;
-	}
-	
+  }
+  
+  private int predictionSquareWidth() {
+    return Board.TILE_SIZE * MAX_PIECE_DIMENSION + PADDING_5;
+  }
+  
+  private void drawTile(Piece piece, int x, int y, Graphics g) {
+    g.setColor(piece.getColor());
+    System.out.println("Printing @" + x + ", " + y);
+    g.fillRect(x, y, TILE_SIZE, TILE_SIZE);
+  }
+  
 }
