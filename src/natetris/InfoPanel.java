59550632772ed26ab4b2f5c90e04fb33702448c5
Streppel <streppels@gmail.com>
2014-09-29 23:10:59 -0300

    added javadoc to InfoPanel class; refactored some variable and method names

--- src/natetris/InfoPanel.java
+++ src/natetris/InfoPanel.java
@@ -7,6 +7,10 @@ import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
+/**
+ * The {@code InfoPanel} class is responsible for showing the current game information, such as
+ * the next piece that is going to be displayed and the current score.
+ */
 public class InfoPanel extends JPanel {
 
   private static final long serialVersionUID = -3849098449596016270L;
@@ -24,11 +28,11 @@ public class InfoPanel extends JPanel {
   private static final int MAX_PIECE_DIMENSION = 4;
   
   /**
-   * Constants used through the panel for layout
+   * Constants used throughout the panel for layout
    */
   private static final int TILE_SIZE = Board.TILE_SIZE;
   private static final int SPACE_BETWEEN_STRINGS = 25;
-  private static final int PADDING_5 = 5;
+  private static final int PADDING = 5;
   private static final int BORDER_WIDTH = Board.BORDER_WIDTH;
   
   /**
@@ -59,7 +63,7 @@ public class InfoPanel extends JPanel {
      * Draws both the rectangle which will hold the next piece to come and
      * the piece itself 
      */
-    g.drawRect(0, 0, predictionSquareWidth(), predictionSquareWidth());
+    g.drawRect(0, 0, predictionBoxWidth(), predictionBoxWidth());
     if (natetris.isGameRunning()) {
       Piece piece = natetris.getNextPiece();
       
@@ -68,8 +72,8 @@ public class InfoPanel extends JPanel {
       for (int x = 0; x < dim; x++) {
         for (int y = 0; y < dim; y++) {
           if (piece.isTile(x, y, 0)) {
-        	  int currX = PADDING_5 + (x * TILE_SIZE);
-        	  int currY = PADDING_5 + (y * TILE_SIZE);
+        	  int currX = PADDING + (x * TILE_SIZE);
+        	  int currY = PADDING + (y * TILE_SIZE);
 	          drawTile(piece, currX, currY, g);
           }
         }
@@ -77,7 +81,7 @@ public class InfoPanel extends JPanel {
     }
     
     g.setColor(FONT_COLOR);
-    offset += predictionSquareWidth(); // strings have to be underneath the prediction square to be visible
+    offset += predictionBoxWidth(); // strings have to be underneath the prediction square to be visible
     
     String points = "Points: ";
     g.drawString(points, 0, offset);
@@ -96,13 +100,12 @@ public class InfoPanel extends JPanel {
 
   }
   
-  private int predictionSquareWidth() {
-    return Board.TILE_SIZE * MAX_PIECE_DIMENSION + PADDING_5;
+  private int predictionBoxWidth() {
+    return Board.TILE_SIZE * MAX_PIECE_DIMENSION + PADDING;
   }
   
   private void drawTile(Piece piece, int x, int y, Graphics g) {
     g.setColor(piece.getColor());
-    System.out.println("Printing @" + x + ", " + y);
     g.fillRect(x, y, TILE_SIZE, TILE_SIZE);
   }
   
